2f42fa1f9f2a70d253943ceb93036f996713b365
wanjochan <wanjochan@gmail.com>
2019-12-02 14:11:56 +0800

    no message

---
+++ build.sh
@@ -0,0 +1,2 @@
+docker build . -t partnernetsoftware/auto_alpine_glibc
+sh testnode.sh -p process.versions
--- dl_node_latest.sh
+++ dl_node_latest.sh
@@ -1,5 +1,5 @@
-#echo DOWNLOAD NODEJS LINUX OF LATEST/V10LTS/V8LTS:
-#
+echo DOWNLOAD NODEJS LINUX OF LATEST/V10LTS/V8LTS:
+
 #cat <<EE > download_nodes.sh
 #wget https://nodejs.org/dist/latest/node-\`wget -qO- https://nodejs.org/dist/latest/ | sed -nE 's|.*>node-(.*)\.pkg</a>.*|\1|p'\`.tar.gz
 #NODE_VERSION_NUM=\`wget -qO- https://nodejs.org/dist/latest-v8.x/ | sed -nE 's|.*>node-(.*)\.pkg</a>.*|\1|p'\` && wget https://nodejs.org/dist/\$NODE_VERSION_NUM/node-\$NODE_VERSION_NUM.tar.gz
@@ -8,9 +8,15 @@
 #EE
 #
 #bash download_nodes.sh
-#
-#
-#
-#docker run -ti -v $PWD:/work/ -w /work/ cmptech/auto_alpine_nodejs_baseline sh -c "/work/alpine_node_latest/node -p 'process.versions'"
 
+#NODE_VERSION_NUM=`wget -qO- https://nodejs.org/dist/latest-v12.x/ | sed -nE 's|.*>node-(.*)\.pkg</a>.*|\1|p'` && wget https://nodejs.org/dist/$NODE_VERSION_NUM/node-$NODE_VERSION_NUM.tar.gz
+
+export NODE_VERSION_NUM=`wget -qO- https://nodejs.org/dist/latest/ | sed -nE 's|.*>node-(.*)\.pkg</a>.*|\1|p'`
+wget -O node-$NODE_VERSION_NUM-linux-x64.tgz https://nodejs.org/dist/$NODE_VERSION_NUM/node-$NODE_VERSION_NUM-linux-x64.tar.gz 
+tar xzvf node-$NODE_VERSION_NUM-linux-x64.tgz
+rm -rf node-latest
+cp -rvf node-$NODE_VERSION_NUM-linux-x64 node-latest
+
+echo docker run -ti -v \$PWD:/work/ -w /work/ partnernetsoftware/auto_alpine_glibc node-latest/bin/node -p process.versions
 docker run -ti -v $PWD:/work/ -w /work/ partnernetsoftware/auto_alpine_glibc node-latest/bin/node -p process.versions
+
